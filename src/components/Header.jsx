a6353051c8bdbb25171265485fbea4e39abfae53
Begayim <b.usubakuova@gmail.com>
2026-02-18 22:26:51 +0600

    Восстановил Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// src/components/Header.jsx
+function Header() {
+  return (
+    <header style={{ backgroundColor: "black", color: "white", padding: "20px", textAlign: "center" }}>
+      <h1>My Header</h1>
+    </header>
+  )
+}
+
+export default Header
+
